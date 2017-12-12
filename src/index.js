fbec534a8f7537472d94379947ac340ff629be8c
Andrew Chavez <chavandrew@gmail.com>
2017-12-12 11:08:12 -0600

    added index.js

--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
+// create a new component--produce some HTML
 
-import App from './components/app';
-import reducers from './reducers';
+const App = () =>  {
+  return <div>Hi!</div>;
+}
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
 
-ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
-  </Provider>
-  , document.querySelector('.container'));
+
+
+
+//taks this component's generated HTML and put it on the page(in the DOM)
+
+ReactDOM.render(<App />, document.querySelector('.container'));
